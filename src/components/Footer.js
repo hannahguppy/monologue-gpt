e097dab1fadf0793b41a53ec134862295572d0db
hannahguppy <80251388+hannahguppy@users.noreply.github.com>
2023-02-09 16:21:26 +0800

    boilerplate

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return(
+    <footer className="py-5 bg-dark">
+      <div className="container">
+        <p className="m-0 text-center text-white">Footer</p>
+      </div>
+    </footer>
+  )
+}
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+// import
+
+export const Home = () => {
+  return(
+    <section className='container main'>
+      Hello
+    </section>
+  )
+}
